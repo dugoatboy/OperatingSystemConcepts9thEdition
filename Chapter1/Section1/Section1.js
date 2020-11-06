a92065a4f13cbeca7bbd9ca25751977d0e734fef
root <root@Patrick-Desktop.localdomain>
2020-11-06 02:47:05 -0500

    boop

--- Chapter1/Section1/Section1.js
+++ Chapter1/Section1/Section1.js
@@ -1,12 +1,9 @@
-var sub1 = false;
-var sub2 = false;
-var sub3 = false;
-
-document.getElementById("sub2").style.display = "none";
-document.getElementById("sub3").style.display = "none";
+var sub1 = getElementById("sub1");
+var sub2 = getElementById("sub2");
+var sub3 = getElementById("sub3");
 
 function checkQ1() {
-    console.log(true);
+    sub2.style.display="block";
 }
 
 function checkQ2() {
