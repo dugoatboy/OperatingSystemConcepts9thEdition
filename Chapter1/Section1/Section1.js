37fa93b2d0d7f3e24e04b4e805ab4d5449a09ea4
root <root@Patrick-Desktop.localdomain>
2020-11-06 04:04:34 -0500

    boop

--- Chapter1/Section1/Section1.js
+++ Chapter1/Section1/Section1.js
@@ -1,28 +1,54 @@
-function checkQ1() {    
+function check_section1_question1() {    
     var ans1 = document.getElementById("q1Answer1");
     var ans2 = document.getElementById("q2Answer2");
     var ans3 = document.getElementById("q3Answer3");
+    var ans = 0;
 
-    var answers = ["hardware management", "user/computer intermediary", "provides a basis for applications"];
-    
-    if (answers.find(ans1) == null) {
-        return;
+    if(ans1 = "manage hardware" || ans1 = "provides basis for application programs" || ans1 = "acts as intermediary between the user and the hardware") {
+        ans++;
     }
 
-    if (answers.find(ans2) == null) {
-        return;
+    if(ans2 = "manage hardware" || ans2 = "provides basis for application programs" || ans2 = "acts as intermediary between the user and the hardware") {
+        ans++;
     }
 
-    if (answers.find(ans3) == null) {
-        return;
+    if(ans3 = "hardware management" || ans3 = "basis for application programs" || ans3 = "intermediary between user and hardware") {
+        ans++;
     }
-    
-    var sub2 = document.getElementById("sub2");
-    sub2.style.display = "block";
+
+    if(ans >= 3) {
+        var s1q2 = document.getElementById("s1q2");
+        s1q2.style.display = "block";
+    }
 }
 
-function checkQ2() {
-    console.log(true);
+function check_section1_question2() {
+    var ans1 = document.getElementById("s1q2Answer1");
+    var ans2 = document.getElementById("s1q2Answer2");
+    var ans3 = document.getElementById("s1q3Answer3");
+    var ans4 = document.getElementById("s1q4Answer4");
+    var ans = 0;
+
+    if(ans1 = "hardware" || ans1 = "operating system" || ans1 = "application programs" || ans1 = "users") {
+        ans++;
+    }
+
+    if(ans2 = "hardware" || ans2 = "operating system" || ans2 = "application programs" || ans2 = "users") {
+        ans++;
+    }
+
+    if(ans3 = "hardware" || ans3 = "operating system" || ans3 = "application programs" || ans3 = "users") {
+        ans++;
+    }
+
+    if(ans4 = "hardware" || ans4 = "operating system" || ans4 = "application programs" || ans4 = "users") {
+        ans++;
+    }
+
+    if(ans >= 4) {
+        var sub3 = document.getElementById("sub2");
+        sub2.style.display = "block";
+    }
 }
 
 function checkQ3() {
