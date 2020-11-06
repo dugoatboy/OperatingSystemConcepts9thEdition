69c59d796a98005aca18fcf769e1ee7a068c09bc
root <root@Patrick-Desktop.localdomain>
2020-11-06 04:09:14 -0500

    boop

--- Chapter1/Section1/Section1.js
+++ Chapter1/Section1/Section1.js
@@ -4,15 +4,15 @@ function check_section1_question1() {
     var ans3 = document.getElementById("q3Answer3");
     var ans = 0;
 
-    if(ans1 = "manage hardware" || ans1 = "provides basis for application programs" || ans1 = "acts as intermediary between the user and the hardware") {
+    if(ans1 == "manage hardware" || ans1 == "provides basis for application programs" || ans1 == "acts as intermediary between the user and the hardware") {
         ans++;
     }
 
-    if(ans2 = "manage hardware" || ans2 = "provides basis for application programs" || ans2 = "acts as intermediary between the user and the hardware") {
+    if(ans2 == "manage hardware" || ans2 == "provides basis for application programs" || ans2 == "acts as intermediary between the user and the hardware") {
         ans++;
     }
 
-    if(ans3 = "hardware management" || ans3 = "basis for application programs" || ans3 = "intermediary between user and hardware") {
+    if(ans3 == "hardware management" || ans3 == "basis for application programs" || ans3 == "intermediary between user and hardware") {
         ans++;
     }
 
@@ -29,19 +29,19 @@ function check_section1_question2() {
     var ans4 = document.getElementById("s1q4Answer4");
     var ans = 0;
 
-    if(ans1 = "hardware" || ans1 = "operating system" || ans1 = "application programs" || ans1 = "users") {
+    if(ans1 == "hardware" || ans1 == "operating system" || ans1 == "application programs" || ans1 == "users") {
         ans++;
     }
 
-    if(ans2 = "hardware" || ans2 = "operating system" || ans2 = "application programs" || ans2 = "users") {
+    if(ans2 == "hardware" || ans2 == "operating system" || ans2 == "application programs" || ans2 == "users") {
         ans++;
     }
 
-    if(ans3 = "hardware" || ans3 = "operating system" || ans3 = "application programs" || ans3 = "users") {
+    if(ans3 == "hardware" || ans3 == "operating system" || ans3 == "application programs" || ans3 == "users") {
         ans++;
     }
 
-    if(ans4 = "hardware" || ans4 = "operating system" || ans4 = "application programs" || ans4 = "users") {
+    if(ans4 == "hardware" || ans4 == "operating system" || ans4 == "application programs" || ans4 == "users") {
         ans++;
     }
 
