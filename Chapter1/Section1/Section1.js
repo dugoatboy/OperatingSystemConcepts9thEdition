f27e758b416d656f9b651f8ef48a7395023f09fb
root <root@Patrick-Desktop.localdomain>
2020-11-06 02:49:53 -0500

    boop

--- Chapter1/Section1/Section1.js
+++ Chapter1/Section1/Section1.js
@@ -1,9 +1,9 @@
-var sub1 = getElementById("sub1");
-var sub2 = getElementById("sub2");
-var sub3 = getElementById("sub3");
+var sub1 = document.getElementById("sub1");
+var sub2 = document.getElementById("sub2");
+var sub3 = document.getElementById("sub3");
 
 function checkQ1() {
-    sub2.style.display="block";
+    sub2.style.display = "block";
 }
 
 function checkQ2() {
