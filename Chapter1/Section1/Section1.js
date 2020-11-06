41082d06f7c8a76c63d8aa38bb185f34520b11e5
root <root@Patrick-Desktop.localdomain>
2020-11-06 02:41:25 -0500

    boop

--- Chapter1/Section1/Section1.js
+++ Chapter1/Section1/Section1.js
@@ -2,8 +2,8 @@ var sub1 = false;
 var sub2 = false;
 var sub3 = false;
 
-document.getElementById("sub2").style.dislay = "none";
-document.getElementById("sub3").style.dislay = "none";
+document.getElementById("sub2").style.display = "none";
+document.getElementById("sub3").style.display = "none";
 
 function checkQ1() {
     console.log(true);
