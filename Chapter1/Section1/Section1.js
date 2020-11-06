112471925eef5d9d5cdd71807af86abc9a00cd9a
root <root@Patrick-Desktop.localdomain>
2020-11-06 03:05:50 -0500

    boop

--- Chapter1/Section1/Section1.js
+++ Chapter1/Section1/Section1.js
@@ -1,10 +1,25 @@
-var sub1 = document.getElementById("sub1");
+function checkQ1() {    
+    var ans1 = document.getElementById("q1Answer1");
+    var ans2 = document.getElementById("q2Answer2");
+    var ans3 = document.getElementById("q3Answer3");
+    var ans = 0;
 
-var sub3 = document.getElementById("sub3");
+    if(ans1 = "manage hardware" || "provides basis for application programs" || "acts as intermediary between the user and the hardware") {
+        ans++;
+    }
 
-function checkQ1() {
-    var sub2 = document.getElementById("sub2");
-    sub2.style.display = "block";
+    if(ans2 = "manage hardware" || "provides basis for application programs" || "acts as intermediary between the user and the hardware") {
+        ans++;
+    }
+
+    if(ans3 = "manage hardware" || "provides basis for application programs" || "acts as intermediary between the user and the hardware") {
+        ans++;
+    }
+
+    if(ans >= 3) {
+        var sub2 = document.getElementById("sub2");
+        sub2.style.display = "block";
+    }
 }
 
 function checkQ2() {
